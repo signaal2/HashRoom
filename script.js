eea3fc11cc8ce717c7b7eceacb65578566ddf455
signaal2 <signaalsignal2@gmail.com>
2026-08-02 17:14:56 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,7 @@ console.log("Telegram:", window.Telegram?.WebApp?.initDataUnsafe);
 const startBtn = document.getElementById("startBtn");
 const balance = document.getElementById("balance");
 startBtn.addEventListener("click", async () => {
-    if (mining) return;
 
-    mining = true;
-    startBtn.innerText = "Mining...";
-
-    miningInterval = setInterval(async () => {
-        btc += 0.00000001;
-
-        balance.innerHTML = btc.toFixed(8) + " BTC";
-
-        await saveBalance();
-    }, 1000);
-});
 let mining = false;
 let btc = 0;
 let miningInterval = null;
@@ -32,6 +20,19 @@ async function saveBalance() {
         {
             balance: btc
         }
+            if (mining) return;
+
+    mining = true;
+    startBtn.innerText = "Mining...";
+
+    miningInterval = setInterval(async () => {
+        btc += 0.00000001;
+
+        balance.innerHTML = btc.toFixed(8) + " BTC";
+
+        await saveBalance();
+    }, 1000);
+});
     );
 }
 async function loadBalance() {
