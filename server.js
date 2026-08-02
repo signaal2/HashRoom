a61eca6572e0349dcf7458a9137361f954b8fd32
signaal2 <signaalsignal2@gmail.com>
2026-08-02 15:39:38 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+// فایل‌های استاتیک
+app.use(express.static(path.join(__dirname, "public")));
 
+// صفحه اصلی
 app.get("/", (req, res) => {
-    res.send("HashRoom Server Online ✅");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
